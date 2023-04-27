7f667df21d02315221ef27908511e982b1532465
uJordy <jordanlubega@hotmail.com>
2023-04-27 21:21:58 +0100

    Added new project folder

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { Text, View, Pressable} from 'react-native';
 
-export default function App() {
+// import { useColorScheme } from 'nativewind';
+
+// export default function App() {
+
+//   const { colorScheme, toggleColorScheme } = useColorScheme();
+//   return (
+//     <View className="flex-1 bg-red-400 items-center justify-center">
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//       <Pressable className="p-4 bg-white dark:bg-black" onPress={toggleColorScheme}>
+//       <Text className="text-black dark:text-white">
+//         Click me to {colorScheme === 'dark' ? 'light' : 'dark'} mode!
+//       </Text>
+//     </Pressable>
+//     </View>
+//   );
+// }
+
+
+import React, {Component} from 'react';
+import { View, Text, Button, SafeAreaView, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomePage from './screens/HomePage';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View className="flex-1  bg-red-500">
+    //   <Text>Hellooooooo!!!</Text>
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+ 
---
+++ screens/HomePage.js
@@ -0,0 +1,57 @@
+import React, { useLayoutEffect } from 'react';
+import { Text, SafeAreaView, Image, View, ScrollView, ImageBackground, TouchableOpacity, Pressable, Button } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+
+function HomePage(props) {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+            
+                {/* Header */}
+                <View>
+                    <Text className="text-3xl font-bold px-6 pt-6 pb-2" >Welcome back, John</Text>
+                    
+                    {/* Upcoming tasks */}
+
+                    <View className="flex flex-row items-center  w-max mx-5 my-3 rounded" >
+                        <Text className="text-2xl">Upcoming Tasks</Text>
+
+                        <View className="bg-orange-500 mx-2 rounded-full">
+                            <Text className= "text-2xl mx-2 rounded-full text-white">5</Text>
+                        </View>
+                    </View>
+                </View>
+
+                {/* Garden tasks */}
+                <View className="h-27 mx-2 rounded-lg">
+                    {/* <Image className='h-5/6 w-28 bg-left float-left absolute left-2 top-2'resizeMode='cover' alignSelf='left' source={require('../app/assets/isometricgarden.png')}/> */}
+                    <Text className="text-2xl text-white font-medium p-1 px-2">Garden</Text> 
+                    {/* List of plants for tasks */}
+                    <View className=" bg-gardenorange w-full flex pb-1">
+                        <TouchableOpacity className="border-2 mx-2 rounded-full h-16  w-16 aspect-square">
+                            <View className="mx-auto my-auto grid rounded-full w-[90%] h-[90%]">
+                                <Image className='w-full h-full rounded-full place-self-center' resizeMode='cover' source={require('../app/assets/exampleplant.webp')}/>
+                                <Image className='w-2/4 h-2/4 absolute z-20 float-right left-0' resizeMode='cover' source={require('../app/assets/drop.png')}/>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+
+export default HomePage;
