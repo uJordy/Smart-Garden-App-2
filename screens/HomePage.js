b5c7fdeba61ba1aeb143989aaf39351f9ac3e209
uJordy <jordanlubega@hotmail.com>
2023-04-28 12:57:39 +0100

    Fixed components and git tracking

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect } from 'react';
-import { Text, SafeAreaView, View, ScrollView, TouchableOpacity, Image} from 'react-native';
+import { Text, SafeAreaView, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 import PlantJob from '../components/PlantJob';
 
 import { useNavigation } from '@react-navigation/native';
@@ -19,18 +19,18 @@ function HomePage(props) {
     return (
         <SafeAreaView>
             <ScrollView>
-            
+
                 {/* Header */}
                 <View>
                     <Text className="text-3xl font-bold px-6 pt-6 pb-2" >Welcome back, John</Text>
-                    
+
                     {/* Upcoming tasks */}
 
                     <View className="flex flex-row items-center  w-max mx-5 my-3 rounded" >
                         <Text className="text-2xl">Upcoming Tasks</Text>
 
                         <View className="bg-orange-500 mx-2 rounded-full">
-                            <Text className= "text-2xl mx-2 rounded-full text-white">5</Text>
+                            <Text className="text-2xl mx-2 rounded-full text-white">5</Text>
                         </View>
                     </View>
                 </View>
@@ -38,29 +38,25 @@ function HomePage(props) {
                 {/* Garden tasks */}
                 <View className="h-27 mx-2 rounded-lg bg-slate-200 ">
                     {/* <Image className='h-5/6 w-28 bg-left float-left absolute left-2 top-2'resizeMode='cover' alignSelf='left' source={require('../app/assets/isometricgarden.png')}/> */}
-                    <Text className="text-2xl font-medium p-1 px-2">Garden</Text> 
+                    <Text className="text-2xl font-medium p-1 px-2">Garden</Text>
                     {/* List of plants for tasks */}
                     <ScrollView className="w-full flex pb-1" horizontal={true}>
-                        
-                    <TouchableOpacity className="border-[3px] border-sky-600 mx-2 rounded-full h-16  w-16 aspect-square">
-        <View className="mx-auto my-auto grid rounded-full w-[90%] h-[90%]">
-          <Image
-            className="w-full h-full rounded-full place-self-center"
-            resizeMode="cover"
-            source={require("../app/assets/exampleplant.webp")}
-          />
-          <Image
-            className="w-2/4 h-2/4 absolute z-20 float-right left-0"
-            resizeMode="cover"
-            source={require("../app/assets/drop.png")}
-          />
-        </View>
-      </TouchableOpacity>
-                        {/* <PlantJob/> */}
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
+                        <PlantJob/>
 
                     </ScrollView>
                 </View>
-                
+
             </ScrollView>
         </SafeAreaView>
     );
