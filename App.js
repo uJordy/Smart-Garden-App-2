1086c4cfd2b3339d692e900b9bbf095cb928f345
uJordy <jordanlubega@hotmail.com>
2023-04-28 16:02:57 +0100

    added plants.js

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text, Button, SafeAreaView, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'; 
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomePage from './screens/HomePage';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomePage} />
-        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Home">
+    //     <Stack.Screen name="Home" component={HomePage} />
+    //     {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+<NavigationContainer>
+<Tab.Navigator>
+  <Tab.Screen name="Home" component={HomeScreen} />
+  <Tab.Screen name="Settings" component={SettingsScreen} />
+</Tab.Navigator>
+</NavigationContainer>
   );
 }
 
 export default App;
 
- 
+
