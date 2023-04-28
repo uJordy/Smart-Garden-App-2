790c0629035c21ec3f3508750a6c31bbc28f6ad3
uJordy <jordanlubega@hotmail.com>
2023-04-28 16:40:27 +0100

    Changed Icons and updated functioality for home page

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { View, Text, Button, SafeAreaView, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomePage from './screens/HomePage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PlantPage from './screens/PlantPage';
+import HomePage from './screens/HomePage';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,11 +22,30 @@ function App() {
     // </NavigationContainer>
 
 <NavigationContainer>
-<Tab.Navigator>
-  <Tab.Screen name="Home" component={HomePage} />
-  <Tab.Screen name="Settings" component={PlantPage} />
-</Tab.Navigator>
-</NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Plants') {
+              iconName = focused ? 'leaf' : 'leaf-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Plants" component={PlantPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
