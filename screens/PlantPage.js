23bd0b77ce29d4b11f4cbf7c48f3375db7818248
uJordy <jordanlubega@hotmail.com>
2023-05-03 20:36:08 +0100

    used imports for svgs using a svg transformer.

--- screens/PlantPage.js
+++ screens/PlantPage.js
@@ -1,30 +1,46 @@
-import React, { useLayoutEffect } from 'react';
-import { Text, View, SafeAreaView, ScrollView, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useLayoutEffect } from "react";
+import { Text, View, SafeAreaView, ScrollView, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
+import Heart from '../assets/svg/heart-o.svg'
 
 function PlantPage(props) {
-
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerShown: false
-    })
+      headerShown: false,
+    });
   }, []);
   return (
     <ScrollView>
       <View className="h-[480px] bg-blue-600 shadow-2xl rounded-bl-[60px] rounded-br-[60px]">
         <View className="flex-1 overflow-hidden rounded-bl-[60px] rounded-br-[60px]">
-          <Image className="absolute w-full h-full z-10" backgroundColor='#dee5db' source={require("../assets/img/exampleplant3.webp")} />
-        </View> 
+          <Image
+            className="absolute w-full h-full z-10"
+            backgroundColor="#dee5db"
+            source={require("../assets/img/exampleplant3.webp")}
+          />
+        </View>
       </View>
       <View className="mx-3 mt-12">
-        <Text className="text-4xl font-bold">Basil</Text>
-        <Text className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui reiciendis tenetur nostrum earum aliquam, maxime quam optio tempora quod temporibus sapiente excepturi dignissimos inventore hic consequatur dolores sunt architecto doloribus.</Text>
+        <View className="flex">
+          <Text className="text-4xl font-bold">Basil</Text>
+          {/* <Image
+          className="h-5 w-5 stroke-blue-500"
+          source={require("../assets/svg/heart-o.svg")}
+          /> */}
+          <Heart width={120} height={40} />
+        </View>
+        <Text className="mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+          reiciendis tenetur nostrum earum aliquam, maxime quam optio tempora
+          quod temporibus sapiente excepturi dignissimos inventore hic
+          consequatur dolores sunt architecto doloribus.
+        </Text>
       </View>
     </ScrollView>
-  )
+  );
 }
 
-export default PlantPage
+export default PlantPage;
