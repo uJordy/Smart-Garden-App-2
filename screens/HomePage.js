fe0b57b634c863a753e70247e37f46a63e74584d
uJordy <jordanlubega@hotmail.com>
2023-11-22 13:19:06 +0000

    Add gitignore

---
+++ screens/HomePage.js
@@ -0,0 +1,69 @@
+import React, { useLayoutEffect } from 'react';
+import { Text, SafeAreaView, View, ScrollView, Image, Platform, FlatList, TouchableOpacity } from 'react-native';
+import HomePageTask from '../components/HomePageTask';
+import { useNavigation } from '@react-navigation/native';
+
+import SoilMoistureSVG from '../assets/svg/SoilMoistureSVG';
+import SunSVG from '../assets/svg/SunSVG';
+import HumiditySVG from '../assets/svg/HumiditySVG';
+
+
+
+
+function HomePage(props) {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, []);
+
+    return (
+        <SafeAreaView className={`${Platform.OS === 'android' ? 'mt-8' : ''}`}>
+            <ScrollView>
+                <Image className="w-16 h-16 mx-auto" source={require("../assets/img/leaf-logo.png")} />
+
+                <View className="bg-orange-500 mx-auto mt-2 rounded-full h-8">
+                    <Text className="text-2xl rounded-full text-white mx-2">5 Tasks</Text>
+                </View>
+
+                <TouchableOpacity className="w-[90%] mx-auto mt-2 bg-slate-50 rounded-2xl">
+                    <Text className="text-xl mx-2 mt-2">Tasks</Text>
+                    <ScrollView>
+                        <HomePageTask />
+                        <HomePageTask />
+                        <HomePageTask />
+                        <HomePageTask />
+                    </ScrollView>
+                </TouchableOpacity>
+
+              
+                <View className="mx-auto w-[90%] mt-2 flex flex-row justify-evenly flex-wrap">
+                    <View className="w-[32%] h-24 bg-blue-200 rounded-3xl aspect-square">
+                        <View className="m-3"><HumiditySVG fill="#60a5fa"/></View>
+                        <Text className="font-semibold ml-2 mt-2 text-gray-800">Humidity</Text>
+                        <Text className="ml-2 text-slate-800">Dry</Text>
+                    </View>
+
+                    <TouchableOpacity className="w-[32%] h-24 bg-orange-200 rounded-3xl aspect-square">
+                        <View className="m-3"><SoilMoistureSVG fill="#fb923c"/></View>
+                        <Text className="font-semibold ml-2 mt-2 text-gray-800">Soil moisture</Text>
+                        <Text className="ml-2 text-slate-800">High</Text>
+                    </TouchableOpacity>
+
+                    <View className="w-[32%] h-24 bg-yellow-200 rounded-3xl aspect-square">
+                    <View className="m-3"><SunSVG fill="#facc15"/></View>
+                        <Text className="font-semibold ml-2 mt-2 text-gray-800">Sunlight</Text>
+                        <Text className="ml-2 text-slate-800">Good</Text>
+                    </View>
+
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+
+export default HomePage;
---
+++ screens/PlantPage.js
@@ -0,0 +1,73 @@
+import React, { useLayoutEffect } from "react";
+import { Text, View, SafeAreaView, ScrollView, Image, TouchableOpacity, TouchableHighlight } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import Heart from "../assets/svg/heart-o.svg";
+
+function PlantPage(props) {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <ScrollView>
+      <View className="h-[480px] bg-slate-200 shadow-2xl shadow-gray-3npx e00 rounded-bl-[60px] rounded-br-[60px]">
+        <View className="flex-1 overflow-hidden rounded-bl-[60px] rounded-br-[60px]">
+          <Image
+            className="absolute w-full h-full z-10"
+            backgroundColor="#dee5db"
+            source={require("../assets/img/exampleplant3.webp")}
+          />
+        </View>
+      </View>
+      <View className="mx-3 mt-12">
+        <View className="flex-1 flex-row justify-between">
+          <Text className="text-4xl font-bold">Not ALong PlantName</Text>
+          <View className="pr-8">
+            <Heart width={35} height={35} fill={"red"} stroke={"red"} />
+          </View>
+        </View>
+        <Text className="text-lg  text-slate-500">Nickname</Text>
+
+        {/* Plant details */}
+        <View className="flex-1 flex-row justify-between mt-3 mx-2">
+          <View>
+            <Text className="text-lg font-bold align-text-bottom leading-tight text-center">Care</Text>
+            <Text className="text-lg align-text-top leading-5 text-center">Easy</Text>
+          </View>
+          <View>
+            <Text className="text-lg font-bold align-text-bottom leading-tight text-center">Sunlight</Text>
+            <Text className="text-lg align-text-top leading-5 text-center">Part-sun</Text>
+          </View>
+          <View>
+            <Text className="text-lg font-bold align-text-bottom leading-tight text-center">Watering</Text>
+            <Text className="text-lg align-text-top leading-5 text-center">Light</Text>
+          </View>
+          <View>
+            <Text className="text-lg font-bold align-text-bottom leading-tight text-center">Humidity</Text>
+            <Text className="text-lg align-text-top leading-5 text-center">High</Text>
+          </View>
+
+        </View>
+
+        <Text className="mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+          reiciendis tenetur nostrum earum aliquam, maxime quam optio tempora
+          quod temporibus sapiente excepturi dignissimos inventore hic
+          consequatur dolores sunt architecto doloribus.
+        </Text>
+
+        <TouchableOpacity className="flex flex-col justify-center rounded-full bg-amber-400 h-12 mt-6 shadow">
+          <Text className="text-white text-center text-2xl font-semibold justify-center">Add to Garden</Text>
+        </TouchableOpacity>
+      </View>
+
+    </ScrollView>
+  );
+}
+
+export default PlantPage;
+  
